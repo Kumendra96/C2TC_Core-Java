193a73684c67a0621868a7699e13208db22463a1
HI <HI@KHUMEN>
2022-03-03 23:52:33 +0530

    first commit

---
+++ My_First_Java_Project/src/com/example/Demo.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Wellcome to My World");
+		System.out.println("My Name is Kumendra");
+	}
+}
