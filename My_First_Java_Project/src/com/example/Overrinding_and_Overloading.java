2b59c2d40cb8a0fd4815e4b4aa72ad109fd7683f
HI <HI@KHUMEN>
2022-03-10 14:56:14 +0530

    commit first

--- My_First_Java_Project/src/com/example/Overrinding_and_Overloading.java
+++ My_First_Java_Project/src/com/example/Overrinding_and_Overloading.java
@@ -28,11 +28,32 @@ class B extends A
 	}
 	
 }
-public class Overrinding_and_Overloading extends B
+class D
+{
+	public D()
+	{
+		
+	}
+	public void m1()
+	{
+		System.out.println("Hello Everyone...");
+	}
+}
+public class Overrinding_and_Overloading
 {
 	public static void main(String[] args) 
 	{
 		A a,a1;
+		B b;
 		a=new A();
+		b=new B();
+		a1=a;
+		a1.m1();
+		a1=b;
+		b.m1();
+		a1.m1();
+		D d =new D();
+		d.m1();		
+		b.m2();
 	}
 }
