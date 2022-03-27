0f5708e9073f5a1778f1ed851b3110cfa73cdbcd
HI <HI@KHUMEN>
2022-03-27 16:15:46 +0530

    First Commit

---
+++ My_First_Java_Project/src/apple/com/AppletDemo.java
@@ -0,0 +1,15 @@
+package apple.com;
+
+import java.awt.Button;
+import java.applet.Applet;
+import java.awt.TextField;
+
+public class AppletDemo extends Applet
+{
+	Button b;
+	TextField t;
+	public void init()
+	{
+		b=new Button("Submit");
+	}
+}
